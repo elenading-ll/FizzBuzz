5ad5a4b178a404cf28b27fe4cbef7838e6851c28
elena-ding <emailelenading@gmail.com>
2025-09-09 22:41:18 -0400

    Successfully ran MultiplesTest and updated README

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,17 +2,18 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(1, 3, 5));
+        System.out.println(multiples(1000, 3, 5));
     }
 
      public static int multiples(int n, int a, int b) {
+        int number = 1;
         int sum = 0;
 
-        while (n < 1000) {
-            if (n % a == 0 || n % b == 0) {
+        while (number < n) {
+            if (number % a == 0 || number % b == 0) {
                 sum++;
             }
-            n++;
+            number++;
         }
         System.out.println(sum);
         return sum;
